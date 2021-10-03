2fed790527ee4cf0a363ffa2ff6625830bdb7b91
evelisko <evelisko@mail.ru>
2021-10-04 02:23:46 +0400

    Файлы риведены в соответствие pep8.

--- Docker/app/IoU.py
+++ Docker/app/IoU.py
@@ -1,16 +1,17 @@
 import numpy as np
 import tensorflow as tf
 
+
 def loss(gt, pred):
     intersections = 0
     unions = 0
-    diff_width = np.minimum(gt[:,0] + gt[:,2], pred[:,0] + pred[:,2]) - np.maximum(gt[:,0], pred[:,2])
-    diff_height = np.minimum(gt[:,1] + gt[:,3], pred[:,1] + pred[:,3]) - np.maximum(gt[:,1], pred[:,3])
+    diff_width = np.minimum(gt[:, 0] + gt[:, 2], pred[:, 0] + pred[:, 2]) - np.maximum(gt[:, 0], pred[:, 2])
+    diff_height = np.minimum(gt[:, 1] + gt[:, 3], pred[:, 1] + pred[:, 3]) - np.maximum(gt[:, 1], pred[:, 3])
     intersection = diff_width * diff_height
-    
+
     # Compute union
-    area_gt = gt[:,2] * gt[:,3]
-    area_pred = pred[:,2] * pred[:,3]
+    area_gt = gt[:, 2] * gt[:, 3]
+    area_pred = pred[:, 2] * pred[:, 3]
     union = area_gt + area_pred - intersection
 
     # Compute intersection and union over multiple boxes
@@ -23,6 +24,7 @@ def loss(gt, pred):
     iou = np.round(intersections / (unions + 1e-8), 4)
     iou = iou.astype(np.float32)
     return iou
+
 
 def IoU(y_true, y_pred):
     iou = tf.py_function(loss, [y_true, y_pred], tf.float32)
--- Docker/app/run_server.py
+++ Docker/app/run_server.py
@@ -10,11 +10,11 @@ from flask import request, jsonify, Flask, redirect
 from logger import Logger
 from object_localization import ObjectLocalization
 from page_classication import PageClassification
-from PIL import Image 
+from PIL import Image
 
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 app = Flask(__name__)
-    
+
 dataframe_path = ""
 loger_patch = ""
 config = {}
@@ -28,8 +28,9 @@ log = Logger(loger_patch)
 log.write('Run Flask Server')
 print('Run Flask Server')
 
-object_localization = ObjectLocalization(config_patch, log) # Для его активации нужно настроить модель.
-page_classify = PageClassification(config_patch, log) # Для его активации нужно настроить модель.
+object_localization = ObjectLocalization(config_patch, log)  # Для его активации нужно настроить модель.
+page_classify = PageClassification(config_patch, log)  # Для его активации нужно настроить модель.
+
 
 @app.route("/", methods=["GET"])
 def general():
@@ -38,7 +39,7 @@ def general():
 
 
 # Здесь выполним обработку POST/GET запроса.
-@app.route("/service", methods=["POST","GET"])  
+@app.route("/service", methods=["POST", "GET"])
 def netflix_films():
     data = {"success": False}
     if request.method == "POST":
@@ -47,31 +48,31 @@ def netflix_films():
             if 'json' not in request.files:
                 print('No json file part')
                 return redirect(request.url)
-              
+
             if 'file' not in request.files:
                 print('No file part')
                 return redirect(request.url)
 
-            img = request.files['file'] # Его мы и будем сохранять.
-            
+            img = request.files['file']  # Его мы и будем сохранять.
+
             request_json = json.loads(request.files['json'].read())
 
             if request_json["method"]:
                 method_name = request_json["method"]
-                print("method_name: ",method_name)
-#----------------------------------------------------------------------------                
+                print("method_name: ", method_name)
+
                 if method_name == "object_localization":
                     data["object_localization"] = object_localization.get_objects_localization(Image.open(img))
-#----------------------------------------------------------------------------
+
                 if method_name == "page_cassify":
                     data["page_cassify"] = page_classify.get_page_type(Image.open(img))
-#----------------------------------------------------------------------------
+
                 print(f'data: {data}')
         except Exception as e:
             log.write(f'Exception: {str(e)}')
             print(f'Exception: {str(e)}')
             data["success"] = False
-    return  jsonify(data)
+    return jsonify(data)
 
 
 if __name__ == "__main__":
